68b986d4511ec3a3beb9640ed45134275e057e44
Jonathan <jonathanjholloway@gmail.com>
2019-09-21 15:59:25 -0500

    Working out several helper functions to assist with calculating the calulator functionality.

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -20,10 +20,172 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Enum
+        public enum Flag
+        {
+            Add,
+            Subtract,
+            Divide,
+            Modulo,
+            Negative,
+            Equals,
+            Default,
+            Multiply,
+        };
+        //Fields 
+        private string _cache = "";
+        private Flag _operation = Flag.Default;
+
+        // Properties
+
+        //My Operation is a enum  in which will help me identify which actions to take. 
+        public Flag Operation { get; set; }
+
+        //This will hold all of the data. That the operation will be held on.  
+        public string Cache
+        {
+            get
+            {
+                return _cache;
+            }
+            set
+            {
+                _cache = value; 
+            }
+                
+        }
+        public double Total { get; set; }
+
+
+
         public MainWindow()
         {
             InitializeComponent();
             //resultLabel.Content = "123345";
+        }
+
+        private void acButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void sevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            string buttonName = ((Button)sender).Name;
+            ButtonHelper(buttonName);
+            
+        }
+
+        /// <summary>
+        /// Used to figure out what action to take on the button that is called. 
+        /// </summary>
+        /// <param name="buttonName">each button has a name and this is used in a switch statement</param>
+        private void ButtonHelper(string buttonName)
+        {
+            switch (buttonName)
+            {
+                //MathOperations Buttons
+                case "acButton":
+                    //reset display
+                    resultLabel.Content = "0";
+                    //reset cache 
+                    Cache = "";
+                    break;
+                case "negativeButton":
+                    resultLabel.Content = $"-{resultLabel.Content}";
+                    //do something with the Total
+                    break;
+                case "modButton":
+                    //set Flag for mod operation. 
+                    Operation = Flag.Modulo;
+                    //place current value on cache. 
+                    CacheResultLabelContent();
+                    break;
+                case "divideButton":
+                    //set Flag for divide operation. 
+                    Operation = Flag.Divide;
+                    //place current value on cache.
+                    CacheResultLabelContent();
+                    break;
+                case "multipyButton":
+                    //set Flag for multiply operation.
+                    Operation = Flag.Multiply;
+                    //place current value on cache.
+                    CacheResultLabelContent();
+                    break;
+                case "minusButton":
+                    //set Flag for subtraction operation.
+                    Operation = Flag.Subtract;
+                    //place current value on cache.
+                    CacheResultLabelContent();
+                    break;
+                case "plusButton":
+                    //set Flag for addition operation.
+                    Operation = Flag.Add;
+                    //place current value on cache.
+                    CacheResultLabelContent();
+                    break;
+                case "equalsButton":
+                    resultLabel.Content = Total.ToString();
+                    break;
+                case "decimalButton":
+                    //if there is not a decimal already added to the string add one.
+                    if(!resultLabel.Content.ToString().Contains("."))
+                    {
+                        resultLabel.Content = $"{resultLabel.Content}.";
+                    }
+                    break;
+                case "zeroButton":
+                    EvalulateNumber(0);
+                    break;
+                case "oneButton":
+                case "twoButton":
+                case "threeButton":
+                case "fourButton":
+                case "fiveButton":
+                case "sixButton":
+                case "sevenButton":
+                case "eightButton":
+                case "nineButton":
+               
+            }
+        }
+        /// <summary>
+        /// Will check the Operation type flag to see if it is set to default. If it is set to default,
+        /// a comparsion is done to see how to add the number to the display. Keeps a running total as well. 
+        /// For instances where the Default is not set it will change the display to the number selected. Total 
+        /// is still updated. 
+        /// </summary>
+        /// <param name="value"></param>
+        private void EvalulateNumber(int value)
+        {
+            if(Operation.CompareTo(Flag.Default) == 0)
+            {
+                //This code block hits if Operation type Flag is set to Default. 
+                if (resultLabel.Content.ToString() == "0")
+                {
+                    //Set the display 
+                    resultLabel.Content = value.ToString();
+                    //set the total
+                    Total = value; 
+
+                }
+                else
+                {
+                    //Set the display
+                    resultLabel.Content = $"{resultLabel.Content}{value}";
+                    //set the total
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// This keeps me from repeating the line of code everywhere in case I need to make a change in the future. 
+        /// </summary>
+        public void  CacheResultLabelContent ()
+        {
+            Cache = resultLabel.Content.ToString();
         }
     }
 }
